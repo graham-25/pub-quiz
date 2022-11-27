2e946df66f5253153f64dbba9443a3e8adcb9c2d
graham-25 <grbills29@googlemail.com>
2022-11-27 11:45:45 +0000

    I have got javaScript to finally do something to the html page its link to.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,13 @@
-//waits for dom to finish loading before running game.
-//Get the button elements and add event listeners to them.
-document.addEventListener("DOMContentLoaded", function() {
-    let button = document.getElementsByTagName("button");
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") == "submit") {
-                alert("You clicked submit");
-            }else{
-                let gameType = this.alert(message?: any): void
-                alert("You clicked" $[gameType]);
-            }
-        })
-    }
-}
+const startButton = document.getElementById('start-btn')
+const questionDisplayElement = document.getElementById('questionDisplay')
 
-function startgame() {
+startButton.addEventListener('click', this.startGame)
 
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionDisplayElement.classList.remove('hide')
+    setNextQuestion();
 }
 
 function displayquection() {
@@ -46,12 +38,12 @@ function displayquection() {
             answer: 2
         },
         {
-            question: "What do monkyes sleep on?"
+            question: "What do monkyes sleep on?",
             choices: ["Floor", "Tree", "Bed", "Bathtub"],
             answer: 3
-        }
+        },
 
-    ];
+    ]
 }
 
 function checkanswer() {
